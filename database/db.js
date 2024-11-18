a39e3419fadbf7d6ac81953fca48a63ac009c79e
Habiba Riaz <154079574+habibariaz@users.noreply.github.com>
2024-11-19 02:15:54 +0500

    Update db.js

--- database/db.js
+++ database/db.js
@@ -5,7 +5,11 @@ const Connection = async (username, password) => {
     const URL = `mongodb+srv://${username}:${password}@blogapp.bqsqi.mongodb.net/?retryWrites=true&w=majority&appName=BlogApp`
 
     try {
-        await mongoose.connect(URL)
+        // await mongoose.connect(URL)
+        await mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
         console.log("DB Connected Successfully!!!")
     } catch (error) {
         console.log("Error in connecting DB!!!!", error)
