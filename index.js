3385a1225e7c1c538b36e09abe44295124bcff9a
Habiba Riaz <154079574+habibariaz@users.noreply.github.com>
2024-11-20 02:52:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,24 @@ import bodyParser from 'body-parser';
 dotenv.config();
 const app = express();
 
+// const corsOptions = {
+//     'Access-Control-Allow-Origin': 'https://blog-app-umber-psi.vercel.app',
+//     'Access-Control-Allow-Credentials': true,
+//     // origin: 'https://blog-app-umber-psi.vercel.app', // Allow frontend origins
+//     'methods': ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+//     'credentials': true, // Allow cookies and Authorization headers
+//     'allowedHeaders': ['Authorization', 'Content-Type'],
+// };
+
 const corsOptions = {
-    Access-Control-Allow-Origin: 'https://blog-app-umber-psi.vercel.app',
-    Access-Control-Allow-Credentials: true,
-    // origin: 'https://blog-app-umber-psi.vercel.app', // Allow frontend origins
+    origin: 'https://blog-app-umber-psi.vercel.app', // Specify the frontend origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
     credentials: true, // Allow cookies and Authorization headers
-    allowedHeaders: ['Authorization', 'Content-Type'],
+    allowedHeaders: ['Authorization', 'Content-Type'], // Specify allowed headers
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // For handling preflight requests
 
 app.use(bodyParser.json({ extended: true }))     //bodyParser.json() specifically parses incoming requests with JSON payloads, making the data available in req.body.
 app.use(bodyParser.urlencoded({ extended: true })) //middleware parses URL-encoded data from the client, such as form submissions where the data is encoded in key-value pairs in the URL format
