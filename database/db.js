7547521308616c82370451fa9aabb601134f121d
Habiba Riaz <154079574+habibariaz@users.noreply.github.com>
2024-11-19 16:22:50 +0500

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose"
 
-const Connection = async (username, password) => {
-
-    const URL = `mongodb+srv://${username}:${password}@blogapp.bqsqi.mongodb.net/?retryWrites=true&w=majority&appName=BlogApp`
+const Connection = async (URL) => {
+// const URL = process.env.MONGODB_URI || `mongodb+srv://${username}:${password}@blogapp.bqsqi.mongodb.net/?retryWrites=true&w=majority&appName=BlogApp`
+    // const URL = `mongodb+srv://${username}:${password}@blogapp.bqsqi.mongodb.net/?retryWrites=true&w=majority&appName=BlogApp`
 
     try {
         // await mongoose.connect(URL)
