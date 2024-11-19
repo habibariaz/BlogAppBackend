acafdee54ea7b323da78bc5ccffe1eef842123b5
Habiba Riaz <154079574+habibariaz@users.noreply.github.com>
2024-11-19 17:12:52 +0500

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,15 +1,11 @@
 import mongoose from "mongoose"
 
 const Connection = async (URL) => {
-// const URL = process.env.MONGODB_URI || `mongodb+srv://${username}:${password}@blogapp.bqsqi.mongodb.net/?retryWrites=true&w=majority&appName=BlogApp`
+
     // const URL = `mongodb+srv://${username}:${password}@blogapp.bqsqi.mongodb.net/?retryWrites=true&w=majority&appName=BlogApp`
 
     try {
-        // await mongoose.connect(URL)
-        await mongoose.connect(URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+        await mongoose.connect(URL)
         console.log("DB Connected Successfully!!!")
     } catch (error) {
         console.log("Error in connecting DB!!!!", error)
